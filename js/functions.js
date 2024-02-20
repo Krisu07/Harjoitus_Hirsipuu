667d8e1383883750d3650bff7e928160a4270d8f
Krisu07 <krista.korento@gmail.com>
2024-02-20 12:58:46 +0200

    Javascript const input, output, span ja const words lisäys koodiin.

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('input')
+const output = document.querySelector('output')
+const span = document.querySelector('span')
+
+const words = [
+     "programming", 
+     "javascript",
+     "database", 
+     "markup", 
+     "framework", 
+     "variable",
+     "stylesheet",
+     "library", 
+     "asynchronous",
+     "hypertext"
+]
